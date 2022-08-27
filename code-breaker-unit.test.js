8a50d9c9435970d02b89642043a91e5600b39b98
valeria <valeria.granada@evertecinc.com>
2022-08-27 09:07:08 -0500

    feat: add algoritm

--- code-breaker-unit.test.js
+++ code-breaker-unit.test.js
@@ -20,4 +20,69 @@ describe('asserts', () => {
         let response = codeBroker.resolve('1789')
         expect(response).toBe('X');
     });
+
+    test('all position different', () => {
+        let response = codeBroker.resolve('2143')
+        expect(response).toBe('----');
+    });
+
+    test('three positions different and assert any one digit', () => {
+        let response = codeBroker.resolve('1423')
+        expect(response).toBe('X---');
+    });
+
+    test('two positions different and assert any two digit', () => {
+        let response = codeBroker.resolve('1243')
+        expect(response).toBe('XX--');
+    });
+
+    test('three positions different', () => {
+        let response = codeBroker.resolve('9143')
+        expect(response).toBe('---');
+    });
+
+    test('two positions different', () => {
+        let response = codeBroker.resolve('8912')
+        expect(response).toBe('--');
+    });
+
+    test('two positions different', () => {
+        let response = codeBroker.resolve('8910')
+        expect(response).toBe('-');
+    });
+
+    test('empty', () => {
+        let response = codeBroker.resolve('7890')
+        expect(response).toBe('');
+    });
+
+    test('assert one digit but is duplicate', () => {
+        let response = codeBroker.resolve('1195')
+        expect(response).toBe('X');
+    });
+
+    test('assert two digit but is duplicate', () => {
+        let response = codeBroker.resolve('1212')
+        expect(response).toBe('XX');
+    });
+
+    test('assert one digit two different', () => {
+        let response = codeBroker.resolve('1329')
+        expect(response).toBe('X--');
+    });
+
+    test('assert one digit one different', () => {
+        let response = codeBroker.resolve('1829')
+        expect(response).toBe('X-');
+    });
+
+    test('assert two digit one different', () => {
+        let response = codeBroker.resolve('1249')
+        expect(response).toBe('XX-');
+    });
+
+    test('assert one digit one different', () => {
+        let response = codeBroker.resolve('1939')
+        expect(response).toBe('X-');
+    });
 })
---
+++ code-breaker.js
@@ -0,0 +1,23 @@
+const secret = '1234';
+
+
+const resolve = (request) => {
+    let correct = '';
+    let coincidence = '';
+    let newSecret = secret.split("");
+
+    for (let i = 0; i < 4; i++) {
+        if (request[i] === newSecret[i]) {
+            correct = correct + "X";
+            newSecret[i] = '/';
+        } else if (newSecret.indexOf(request[i]) !== -1) {
+            coincidence = coincidence + "-";
+            newSecret[newSecret.indexOf(request[i])] = '/';
+        }
+    }
+
+    return correct + coincidence;
+}
+module.exports = {
+    resolve
+}
